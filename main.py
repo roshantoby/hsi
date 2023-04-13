1c338b0a079fd300f38f4eb1193a756d172e094c
Roshan <roshantoby@gmail.com>
2023-04-13 17:28:17 +0100

    Features: Added feature to upload file for analysis.

--- main.py
+++ main.py
@@ -1,26 +1,89 @@
-from dash import Dash, html, dcc, callback, Output, Input
-import plotly.express as px
-import pandas as pd
+import base64
+import io
+
 import dash_bootstrap_components as dbc
 import numpy as np
+import pandas as pd
+import plotly.express as px
+from dash import Dash, html, dcc, callback, Output, Input, callback_context, no_update, State
+from dash.exceptions import PreventUpdate
 
-chicken_hsi = np.load('output/chicken_hsi_im.npy')
-chicken_pca = np.load('output/chicken_pca_im.npy')
-wavelengths = np.genfromtxt('input/chicken_HYSPEC.csv', delimiter='\t')[0]
+from pca import run_pca
 
 app = Dash(__name__)
 
 app.layout = html.Div([
     html.H1(children='PCA Analysis', style={'textAlign': 'center'}),
-    dbc.Row([
-        dbc.Col([dcc.Dropdown([i for i in range(1, chicken_pca.shape[2] + 1)], 1,
-                              id='dropdown-selection'),
-                dcc.Graph(id='graph-content',
-                          hoverData={'points': [{'y': 0, 'x': 0}]}
-                          )], width="auto"),
-        dbc.Col(dcc.Graph(id='spectral-content'), width="auto")
-    ])
+    dbc.Row(
+        [dcc.Upload(id='upload-data',
+                    children=['Drag and Drop or ',
+                              html.A('Select Hyperspectral File')]
+                    , style={
+                'width': '50%',
+                'height': '60px',
+                'lineHeight': '60px',
+                'borderWidth': '1px',
+                'borderStyle': 'dashed',
+                'borderRadius': '5px',
+                'textAlign': 'center'
+            },
+                    multiple=False,
+                    max_size=-1,
+                    )
+         ]
+    ),
+    dbc.Row([dcc.Input(id="height-input", type="number", placeholder="enter image height"),
+             dcc.Input(id="width-input", type="number", placeholder="enter image width")]),
+    dbc.Row(html.Button('Run PCA', id='pca-button', n_clicks=0)),
+    dbc.Row(dcc.Loading(
+        id="loading-1",
+        type="default",
+        children=html.Div(id="loading-output-1")
+    )),
+    dbc.Row(dcc.Graph(id='spectral-content'))
 ])
+
+
+@callback(
+    [Output("upload-data", "children"),
+     Output("loading-output-1", "children")],
+    [Input("upload-data", 'contents'),
+     Input("pca-button", 'n_clicks'),
+     Input("height-input", "value"),
+     Input("width-input", "value")],
+    State('upload-data', 'filename'),
+)
+def render_pca(upload_file, pca_button, height, width, filename):
+    changed_id = [p['prop_id'] for p in callback_context.triggered][0]
+    if upload_file is None:
+        raise PreventUpdate
+
+    if "pca-button" not in changed_id:
+        return filename, no_update
+    else:
+
+        content_type, content_string = upload_file.split(",")
+        decoded = base64.b64decode(content_string)
+        hsi_data = np.genfromtxt(io.StringIO(decoded.decode("utf-8")), delimiter='\t')
+        run_pca(hsi_data, height, width)
+
+        global pca_im
+        global wavelengths
+        global hsi_im
+
+        pca_im = np.load('output/pca_im.npy')
+        hsi_im = np.load('output/hsi_im.npy')
+        wavelengths = hsi_data[0]
+
+        graphs_children = [
+            dbc.Col([dcc.Dropdown([i for i in range(1, pca_im.shape[2] + 1)], 1,
+                                  id='dropdown-selection'),
+                     dcc.Graph(id='graph-content',
+                               hoverData={'points': [{'y': 0, 'x': 0}]}
+                               )], width="auto")
+        ]
+
+    return filename, graphs_children
 
 
 @callback(
@@ -28,7 +91,7 @@ app.layout = html.Div([
     Input('dropdown-selection', 'value')
 )
 def update_pca_im(value):
-    pca_sl = chicken_pca[:, :, value - 1]
+    pca_sl = pca_im[:, :, value - 1]
     return px.imshow(pca_sl)
 
 
@@ -38,10 +101,10 @@ def update_pca_im(value):
 )
 def update_spectral_graph(hoverData):
     df = pd.DataFrame({'wavelength': wavelengths,
-                       'intensity': chicken_hsi[hoverData['points'][0]['y'], hoverData['points'][0]['x'], :]})
+                       'intensity': hsi_im[hoverData['points'][0]['y'], hoverData['points'][0]['x'], :]})
 
     return px.line(df, x='wavelength', y='intensity', title='Hyperspectral Data of Pixel')
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=True, threaded=True)
--- pca.py
+++ pca.py
@@ -1,16 +1,16 @@
 from sklearn.decomposition import PCA
 import numpy as np
 
-chicken = np.genfromtxt('input/chicken_HYSPEC.csv', delimiter='\t')
-chicken_hsi = chicken[1:]
 
-pca = PCA(n_components=5)
+def run_pca(hsi_data, height, width):
+    hsi_im = hsi_data[1:]
 
-chicken_pca = pca.fit_transform(chicken_hsi)
-chicken_hsi_im = chicken_hsi.reshape(100, 100, -1)
-chicken_pca_im = chicken_pca.reshape(100, 100, -1)
-np.save('output/chicken_pca_im.npy', chicken_pca_im)
-np.save('output/chicken_hsi_im.npy', chicken_hsi_im)
+    pca = PCA(n_components=5)
 
-print('done')
+    hsi_pca = pca.fit_transform(hsi_im)
+    hsi_im = hsi_im.reshape(height, width, -1)
+    hsi_pca = hsi_pca.reshape(height, width, -1)
+    np.save('output/pca_im.npy', hsi_pca)
+    np.save('output/hsi_im.npy', hsi_im)
 
+    return None
